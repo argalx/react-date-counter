910a8e76c3f20b34165419bd2da915eccc511db7
Arvin Bonggal <arvinbonggal@gmail.com>
2025-04-21 16:56:39 +0800

    Add Step component and initial app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Step />
+      <div>
+        <button>&minus;</button>
+        <span>Count: {counter}</span>
+        <button>&#43;</button>
+      </div>
+      <div>
+        <p>Today is Mon June 21 2027</p>
+      </div>
+    </div>
+  );
+}
+
+function Step() {
+  const [step, setStep] = useState(1);
+
+  // This function is called when the decrement button is clicked
+  function handleStepDecrement() {
+    if (step > 1) {
+      setStep((prevStep) => prevStep - 1);
+    }
+  }
+
+  // This function is called when the increment button is clicked
+  function handleStepIncrement() {
+    setStep((prevStep) => prevStep + 1);
+  }
+
+  return (
+    <div>
+      <button onClick={handleStepDecrement}>&minus;</button>
+      <span>Step: {step}</span>
+      <button onClick={handleStepIncrement}>&#43;</button>
     </div>
   );
 }
