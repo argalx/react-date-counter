a276112be4ff9c3cd481f9ac811027f31d76b5da
Arvin Bonggal <arvinbonggal@gmail.com>
2025-04-22 14:43:30 +0800

    My Answer with a little bit of peaking for the date and message part

--- src/App.js
+++ src/App.js
@@ -2,23 +2,6 @@ import { useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
-
-  return (
-    <div>
-      <Step />
-      <div>
-        <button>&minus;</button>
-        <span>Count: {counter}</span>
-        <button>&#43;</button>
-      </div>
-      <div>
-        <p>Today is Mon June 21 2027</p>
-      </div>
-    </div>
-  );
-}
-
-function Step() {
   const [step, setStep] = useState(1);
 
   // This function is called when the decrement button is clicked
@@ -33,11 +16,51 @@ function Step() {
     setStep((prevStep) => prevStep + 1);
   }
 
+  // This function is called when the counter decrement button is clicked
+  function handleCounterDecrement() {
+    setCounter((prevCounter) => prevCounter - step);
+  }
+
+  // This function is called when the counter increment button is clicked
+  function handleCounterIncrement() {
+    setCounter((prevCounter) => prevCounter + step);
+  }
+
   return (
     <div>
-      <button onClick={handleStepDecrement}>&minus;</button>
-      <span>Step: {step}</span>
-      <button onClick={handleStepIncrement}>&#43;</button>
+      <div>
+        <button onClick={handleStepDecrement}>&minus;</button>
+        <span>Step: {step}</span>
+        <button onClick={handleStepIncrement}>&#43;</button>
+      </div>
+      <div>
+        <button onClick={handleCounterDecrement}>&minus;</button>
+        <span>Count: {counter}</span>
+        <button onClick={handleCounterIncrement}>&#43;</button>
+      </div>
+      <Message counter={counter} />
+    </div>
+  );
+}
+
+function Message({ counter }) {
+  const date = new Date();
+  date.setDate(date.getDate() + counter);
+
+  // Today
+  // Days from today
+  // Days ago
+
+  return (
+    <div>
+      <span>
+        {counter === 0
+          ? "Today is "
+          : counter > 0
+          ? `${counter} days from today is `
+          : `${Math.abs(counter)} days ago is `}
+      </span>
+      <span>{date.toDateString()}</span>
     </div>
   );
 }
